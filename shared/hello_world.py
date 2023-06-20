d661634a8cf2a818513fbe0d9fb5d86d4c12076b
senioravo <137220932+senioravo@users.noreply.github.com>
2023-06-20 16:43:56 -0400

    session2

--- shared/hello_world.py
+++ shared/hello_world.py
@@ -1,4 +1,25 @@
+import os
+
+os.system("cls")
+
 print("Hello Wolrd :)")
 
 # this is a comentary
+<<<<<<< HEAD
+
+input("hola\n->")
+
+#wena compare
+
+#saludo = input("Ingrese su saludo compare: fakiu ")
+apellido = ["herrera","riquelme","alcaino"]
+
+
+
+#presentense aqui abajo con nombre, edad, estado civil
+
+
+=======
 #hola
+
+>>>>>>> 20a0b577bc9b67f8e580c75f9033c6832d1a3911
